35f4c8cf8257932628f305f408e1c8f215eb6897
Andrei96 <andrei.nike2012@gmail.com>
2017-02-19 11:28:48 +0200

    update second lab and add third lab

---
+++ API/lab3/1/result/result/main.cpp
@@ -0,0 +1,164 @@
+#include <windows.h>
+
+//Створення прототипу функцї вікна, яка буде визначена нижче
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//оголошення рядка-імя програми
+
+char szProgName[] = "Імя програми";
+char szIconName[] = "";
+char szCursorName[] = "";
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR
+
+lpszCmdLine, int nCmdShow)
+
+{
+
+HWND hWnd;
+
+MSG lpMsg;
+
+WNDCLASS w; //створення екземпляру структури WNDCLASS
+
+//заповнення цієї структури
+
+w.lpszClassName = szProgName; //імя програми - ооголошено вище
+
+w.hInstance=hInstance; //ідентифікатор поточного додатку
+
+w.lpfnWndProc=WndProc; //вказівник на функцію вікна
+
+//w.hCursor=LoadCursor(NULL, IDC_ARROW); завантаження курсору
+w.hCursor = LoadCursor(NULL, IDC_SIZEWE);
+
+//w.hIcon=0; іконки поки немає
+w.hIcon = LoadIcon(hInstance, szIconName);
+
+
+w.lpszMenuName=0; // меню поки не буде
+
+w.hbrBackground=(HBRUSH)GetStockObject(WHITE_BRUSH); //колір фону вікна
+
+w.style=CS_HREDRAW|CS_VREDRAW; //стиль - перемалювання по х і по у
+
+w.cbClsExtra=0;
+
+w.cbWndExtra=0;
+
+//Якщо не вдалося зареєструвати клас вікна – виходимо
+
+if(!RegisterClass(&w))
+
+return 0;
+
+//Створення вікна в памяті, заповненням аргументів CreateWindow
+
+hWnd=CreateWindow(szProgName, //Імя програми
+
+"Blizniuk A. O.", //Заголовок вікна
+
+WS_OVERLAPPEDWINDOW, //Стиль вікна – таке, що перекривається
+
+100, //положення вікна на екрані по х
+
+100, // положення по у
+
+500, //ширина
+
+400, //висота
+
+(HWND)NULL, //ідентифікатор батьківського вікна
+
+(HMENU)NULL, // ідентифікатор меню
+
+(HINSTANCE)hInstance, // ідентифікатор екземпляру програми
+
+(HINSTANCE)NULL); //відсутність додаткових параметрів
+
+//Виводимо вікно з памяті на екран
+
+ShowWindow(hWnd, nCmdShow);
+
+//Оновлення вмісту вікна
+
+//8
+
+UpdateWindow(hWnd);
+
+//Цикл обробки повідомлень
+
+while(GetMessage(&lpMsg, hWnd, 0, 0)) { //Отримуємо повідомлення з черги
+
+TranslateMessage(&lpMsg); //Перетворення повідомлення клавіш у символи
+
+DispatchMessage(&lpMsg); //Передача повідомлень відповідній функції вікна
+
+}
+
+return(lpMsg.wParam);
+
+}
+
+//Функція вікна
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT messg,
+
+WPARAM wParam, LPARAM lParam)
+
+{
+
+HDC hdc; //створення контексту пристрою
+
+PAINTSTRUCT ps; //створення екземпляру структури графічного виведення
+
+//Цикл обробки повідомлень
+
+switch(messg)
+
+{
+
+//повідомлення малювання
+
+case WM_PAINT :
+
+//початок малювання
+
+hdc=BeginPaint(hWnd, &ps);
+
+//вставка свого тексту:
+
+TextOut(hdc, 150,150, "Привіт, WIN 32 API!!!!", 19);
+
+//оновлюємо вікно
+
+ValidateRect(hWnd, NULL);
+
+//завершення малювання
+
+EndPaint(hWnd, &ps);
+
+break;
+
+//повідомлення виходу - руйнування вікна
+
+case WM_DESTROY:
+
+PostQuitMessage(0); //Надсилання повідомлення виходу з кодом 0 - нормальне
+
+//завершення
+
+break;
+
+default:
+
+return(DefWindowProc(hWnd, messg, wParam, lParam)); //звільнення черги додатку
+
+//від нерозпізнаних повідомлень
+
+}
+
+return 0;
+
+}
